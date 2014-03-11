b2041d16e0d84b4fa49619e047fca92fd892166f
Jolene Aw <joleneaw@Jolenes-MacBook-Air.local>
2014-03-11 23:19:16 +0800

    Initial Commit

---
+++ SingASong/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SingASong
+//
+//  Created by Jolene Aw on 3/11/14.
+//  Copyright (c) 2014 Jolene Aw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SingASong/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SingASong
+//
+//  Created by Jolene Aw on 3/11/14.
+//  Copyright (c) 2014 Jolene Aw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end

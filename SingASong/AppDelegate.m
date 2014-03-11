882f10182c336f92dc5d9edd7f7f81eecbb483ba
joleneawy <jolene0613@gmail.com>
2014-03-12 01:00:45 +0800

    Added for Facebook login

--- SingASong/AppDelegate.m
+++ SingASong/AppDelegate.m
@@ -6,14 +6,46 @@
 //  Copyright (c) 2014 Jolene Aw. All rights reserved.
 //
 
+#import <FacebookSDK/FacebookSDK.h>
 #import "AppDelegate.h"
+#import "FBFunLoginDialog.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    // Load the FBProfilePictureView
+    // You can find more information about why you need to add this line of code in our troubleshooting guide
+    // https://developers.facebook.com/docs/ios/troubleshooting#objc
+    [FBProfilePictureView class];
+    
+    // Create a LoginUIViewController instance where the login button will be
+    FBFunLoginDialog *loginUIViewController = [[FBFunLoginDialog alloc] init];
+    
+    // Set loginUIViewController as root view controller
+    [[self window] setRootViewController:loginUIViewController];
+    
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
     return YES;
+}
+
+// In order to process the response you get from interacting with the Facebook login process,
+// you need to override application:openURL:sourceApplication:annotation:
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+    
+    // Call FBAppCall's handleOpenURL:sourceApplication to handle Facebook app responses
+    BOOL wasHandled = [FBAppCall handleOpenURL:url sourceApplication:sourceApplication];
+    
+    // You can add your app-specific url handling code here if needed
+    
+    return wasHandled;
 }
 							
 - (void)applicationWillResignActive:(UIApplication *)application
---
+++ SingASong/FBFunLoginDialog.h
@@ -0,0 +1,15 @@
+//
+//  FBFunLoginDialog.h
+//  SingASong
+//
+//  Created by Jolene Aw on 3/11/14.
+//  Copyright (c) 2014 Jolene Aw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <FacebookSDK/FacebookSDK.h>
+
+
+@interface FBFunLoginDialog : UIViewController <FBLoginViewDelegate>
+
+@end
